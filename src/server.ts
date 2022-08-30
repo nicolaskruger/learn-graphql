237c0935b7abdde23c360682bfa06d02a4636db7
nkruger <nicolas.nkruger@gmail.com>
2022-08-30 19:01:34 -0300

    query

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,31 @@
-import type {User} from '@dto/user';
+import {buildSchema} from 'graphql';
 import express from 'express';
+import {graphqlHTTP} from 'express-graphql';
+
+const schema = buildSchema(`
+	type Query {
+    	hello: String,
+		by: String,
+		neymar: String
+		user: User
+  	}
+	type User {
+		name: String
+	}
+`);
+
+const rootValue = {
+	neymar: (pronome: string) => `neymar eh ${pronome}`,
+	hello: () => 'Hello world!',
+	by: () => 'by',
+	user: () => ({name: 'sexo'}),
+};
 
 const app = express();
 
-app.get('/', (req, res) => {
-	const user: User = {
-		name: 'sergio',
-	};
-
-	return res.json(user);
-});
-
+app.use('/graphql', graphqlHTTP({
+	schema,
+	rootValue,
+	graphiql: true,
+}));
 app.listen(3333);
